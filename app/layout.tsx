79f498fcd2aeff8f62aa66bb9d7fd88218988caf
digitalfix-bd <digitalfixbd@gmail.com>
2025-11-21 13:12:49 +0600

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'EMS',
+  description: 'EMS APP',
+  generator: 'EMS WEB APP',
   icons: {
     icon: [
       {
